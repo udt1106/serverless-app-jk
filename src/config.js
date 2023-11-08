5e1f77bf6b7ba4bf2e17046d6cc6fe1d9c455ab1
Jihyun Kim <developer1106@gmail.com>
2023-11-07 21:54:49 -0500

    cleared cache

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export const serverlessAppFunctionHTTP = () => {
-    return "https://*****.execute-api.us-east-1.amazonaws.com/serverlessAppFunctionHTTP";
-}
-
-export const serverlessAppFunction2 = () => {
-    return "https://*****.execute-api.us-east-1.amazonaws.com/default/serverlessAppFunction2";
-}
